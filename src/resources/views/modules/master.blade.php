1b41cd464747b11d5f18a953014518aa13691413
mark <Samark.Cha@ascendcorp.com>
2019-03-30 00:46:37 +0700

    remove css

--- src/resources/views/modules/master.blade.php
+++ src/resources/views/modules/master.blade.php
@@ -161,58 +161,7 @@
         }
 
         .badge.badge-level-all,
-        .box.level-all {
-            background-color: {{ log_styler()->color('all') }};
-        }
 
-        .badge.badge-level-emergency,
-        .box.level-emergency {
-            background-color: {{ log_styler()->color('emergency') }};
-        }
-
-        .badge.badge-level-alert,
-        .box.level-alert {
-            background-color: {{ log_styler()->color('alert') }};
-        }
-
-        .badge.badge-level-critical,
-        .box.level-critical {
-            background-color: {{ log_styler()->color('critical') }};
-        }
-
-        .badge.badge-level-error,
-        .box.level-error {
-            background-color: {{ log_styler()->color('error') }};
-        }
-
-        .badge.badge-level-warning,
-        .box.level-warning {
-            background-color: {{ log_styler()->color('warning') }};
-        }
-
-        .badge.badge-level-notice,
-        .box.level-notice {
-            background-color: {{ log_styler()->color('notice') }};
-        }
-
-        .badge.badge-level-info,
-        .box.level-info {
-            background-color: {{ log_styler()->color('info') }};
-        }
-
-        .badge.badge-level-debug,
-        .box.level-debug {
-            background-color: {{ log_styler()->color('debug') }};
-        }
-
-        .badge.empty,
-        .box.empty {
-            background-color: {{ log_styler()->color('empty') }};
-        }
-
-        .badge.badge-env {
-            background-color: #6A1B9A;
-        }
     </style>
 </head>
 <body>
