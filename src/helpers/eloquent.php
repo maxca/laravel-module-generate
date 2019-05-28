e9253fba61851d81e49f6bbc9332b4cbd412cbca
maxca789 <samarkchsngn@gmail.com>
2019-05-29 00:48:00 +0700

    add auto clear cache

--- src/helpers/eloquent.php
+++ src/helpers/eloquent.php
@@ -30,3 +30,44 @@ if (!function_exists('arrayConcat')) {
         return $result;
     }
 }
+
+if (!function_exists('clearCache')) {
+    /**
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    function clearCache()
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL            => env('API_CLOUDFLARE'),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => "",
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => "POST",
+            CURLOPT_POSTFIELDS     => "{\"purge_everything\":true}",
+            CURLOPT_HTTPHEADER     => array(
+                "Content-Type: application/json",
+                "Postman-Token: 8bebf8e3-8341-44ad-8c01-cc40edb915cd",
+                "X-Auth-Email: samarkchsngn@gmail.com",
+                "X-Auth-Key: 37defcdeddee0647d10f6ea514c78734cd5d1",
+                "cache-control: no-cache"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err      = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            return "cURL Error #:" . $err;
+        } else {
+            \Log::info('clearCache::response::' . json_encode($response));
+            return $response;
+        }
+    }
+}
